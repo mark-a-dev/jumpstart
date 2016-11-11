36f9d70e43fb449d3482baf32af8369e9cd78a9b
Mark-A-A <markawai01@gmail.com>
2016-11-11 01:43:12 -0500

    startimg debuggig work and finishing off day 3 work

--- 1-week/4-day/w1d4-pair-problems.rb
+++ 1-week/4-day/w1d4-pair-problems.rb
@@ -4,12 +4,24 @@
 # super long) ruby method names.
 
 def rubyify(sentence)
+  letters = "abcdefghijklmnopqrstuvwxyz"
+  new_sentence = sentence.downcase
+  new_sentence.each_char do |char|
+    if char == " "
+      new_sentence[char] = "_"
+    end
+  end
+  puts new_sentence
+  new_sentence = new_sentence.delete(".")
+  new_sentence = new_sentence.delete("?")
+  puts new_sentence
+  new_sentence
 end
 
-puts "---------Rubyify----------"
-puts rubyify("Leave the gun take the canoli.") == "leave_the_gun_take_the_canoli"
-puts rubyify("Driver roll up the partition please") == "driver_roll_up_the_partition_please"
-puts rubyify("Wanna get Cheeseboard pizza?") == "wanna_get_cheeseboard_pizza"
+# puts "---------Rubyify----------"
+# puts rubyify("Leave the gun take the canoli.") == "leave_the_gun_take_the_canoli"
+# puts rubyify("Driver roll up the partition please") == "driver_roll_up_the_partition_please"
+# puts rubyify("Wanna get Cheeseboard pizza?") == "wanna_get_cheeseboard_pizza"
 
 
 
@@ -23,6 +35,26 @@ puts rubyify("Wanna get Cheeseboard pizza?") == "wanna_get_cheeseboard_pizza"
 # returns a new hash with all the key / value pairs swapped. Do not use Hash#invert.
 
 def key_value_swap(hash)
+  swapped_hash = Hash.new
+
+  hash.each { |k,v| swapped_hash[v] = k}
+
+
+  swapped_hash
+  # old_keys = hash.keys
+  # puts old_keys
+  # old_values = hash.values
+  # puts old_values
+
+  #
+  # old_values.each { |element1|
+  #   old_keys.each { |element2|
+  #
+  #     swapped_hash[element1] = element2
+  #   }
+  # }
+
+swapped_hash
 end
 
 puts "---------Key Value Swap----------"
